3fd34369956025ac5c1c0203721fd720c187c3f1
David Wheeler <dawheel@davidwheeler.me.uk>
2019-08-20 22:21:28 +0100

    completed a blog post list with template.

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    qs = Post.objects.all() # query set 'qs'
-    template_name = 'blog/post_list.html'
-    context = {'posts': qs} #python dictionary - key is 'posts'; value is 'qs'
+    qs = Post.objects.filter(published_date__lte=timezone.now())
+    # queryset 'qs' with field lookup using __ (double underscore) & lte (later than or equal to
+    template_name = 'post_list.html'
+    context = {'posts': qs}
+    # context is python dictionary(key:value) - key is 'posts'; value is 'qs'
     return render(request, template_name, context)
     
