fc4ebc71f6a61af5f6c443e1c958bcb70ea3ddc4
David Wheeler <dawheel@davidwheeler.me.uk>
2019-08-20 14:51:40 +0100

    created a blog post list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    qs = Post.objects.all() # query set 'qs'
+    template_name = 'blog/post_list.html'
+    context = {'posts': qs} #python dictionary - key is 'posts'; value is 'qs'
+    return render(request, template_name, context)
+    
