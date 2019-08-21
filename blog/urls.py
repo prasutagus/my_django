513ef5cecb06db88bd88c048c61a795898e928a6
David Wheeler <dawheel@davidwheeler.me.uk>
2019-08-21 13:44:07 +0100

    removed unnecessary model fields and finished creating the basic CRUD elements for templates, views and urls.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('blog/new', views.post_create, name='post_create'),
+    path('blog/<int:pk>/', views.post_detail, name='post_detail'),
+    path('blog/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('blog/<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
-    qs = Post.objects.filter(published_date__lte=timezone.now())
-    # queryset 'qs' with field lookup using __ (double underscore) & lte (later than or equal to
+    qs = Post.objects.all()
+    # queryset 'qs' bringing all posts
     template_name = 'post_list.html'
     context = {'posts': qs}
     # context is python dictionary(key:value) - key is 'posts'; value is 'qs'
     return render(request, template_name, context)
-    
+
+# CRUD
+def post_create(request): # create
+    form = PostForm(request.POST)
+    if form.is_valid():
+        # post = form.save(commit=False) # to stop the save until dated 
+        form.save()
+        return redirect('post_list')
+    else:
+        form = PostForm()
+
+    template_name = 'post_create.html'
+    context = {'form': form}
+    return render(request, template_name, context)
+
+def post_detail(request, pk): # retrieve
+    post = get_object_or_404(Post, pk=pk)
+    template_name = 'post_detail.html'
+    return render(request, template_name, {'post':post})
+
+def post_edit(request, pk): # update
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            # post = form.save(commit=False) # to stop the save until dated 
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    template_name = 'post_edit.html'
+    return render(request, template_name, {'form':form})
+
+def post_delete(request, pk): # delete
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
