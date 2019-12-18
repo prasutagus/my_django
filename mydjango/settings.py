0b44564a02af7b2d3929907bc3d1a63c26a9d911
David <david@davidwheeler.me.uk>
2019-12-18 20:54:09 +0000

    Updated all to Python 3.8.

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-GB'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
